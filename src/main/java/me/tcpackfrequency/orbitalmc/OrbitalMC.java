a02d698b44961b96dbb40f6797c3242110140f0a
Tcp <tcpackfrequency@gmail.com>
2018-03-13 16:43:01 +0000

    Initial commit

---
+++ src/main/java/me/tcpackfrequency/orbitalmc/OrbitalMC.java
@@ -0,0 +1,11 @@
+package me.tcpackfrequency.orbitalmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OrbitalMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+}
